4d77def2664b542e9faaf4443896601c1d0a1af2
kibumbing <110390520+kibumbing@users.noreply.github.com>
2022-09-05 14:00:20 +0900

    Add files via upload

---
+++ bert_1.sh
@@ -0,0 +1,1 @@
+python klue_classification.py
---
+++ klue_classification.py
@@ -0,0 +1,172 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+import gluonnlp as nlp
+import numpy as np
+from tqdm import tqdm, tqdm_notebook
+
+from kobert.utils import get_tokenizer
+from kobert.pytorch_kobert import get_pytorch_kobert_model
+
+from transformers import AdamW
+from transformers.optimization import get_cosine_schedule_with_warmup
+
+from bert_model import *
+
+from datasets import load_dataset
+import pandas as pd
+
+dataset = load_dataset("klue", 'ynat')
+
+train_klue = pd.DataFrame({'title': dataset['train']['title'],
+                           'label': dataset['train']['label']})
+
+test_klue = pd.DataFrame({'title': dataset['validation']['titleklue_classifi'],
+                          'label': dataset['validation']['label']})
+
+device = torch.device("cuda:0")
+bertmodel, vocab = get_pytorch_kobert_model()
+
+train_data = []
+for q, label in zip(train_klue[:]['title'], train_klue[:]['label']):
+    data = []
+    data.append(q)
+    # 0:IT과학 1:경제 2:사회 3:생활문화
+    if label == 0 or label == 1 or label == 2 or label == 3:
+        data.append(str(0))
+    # 4:세계 5:스포츠 6:정치
+    elif label == 4 or label == 5 or label == 6:
+        data.append(str(1))
+    train_data.append(data)
+
+test_data = []
+for q, label in zip(test_klue[:]['title'], test_klue[:]['label']):
+    data = []
+    data.append(q)
+    # 0:IT과학 1:경제 2:사회 3:생활문화
+    if label == 0 or label == 1 or label == 2 or label == 3:
+        data.append(str(0))
+    # 4:세계 5:스포츠 6:정치
+    elif label == 4 or label == 5 or label == 6:
+        data.append(str(1))
+    test_data.append(data)
+
+# Setting parameters
+max_len = 64
+batch_size = 64
+warmup_ratio = 0.1
+num_epochs = 5
+max_grad_norm = 1
+log_interval = 200
+learning_rate = 5e-5
+
+#토큰화
+tokenizer = get_tokenizer()
+tok = nlp.data.BERTSPTokenizer(tokenizer, vocab, lower=False)
+
+data_train = BERTDataset(train_data, 0, 1, tok, max_len, True, False)
+data_test = BERTDataset(test_data, 0, 1, tok, max_len, True, False)
+
+train_dataloader = torch.utils.data.DataLoader(data_train, batch_size=batch_size, num_workers=5)
+test_dataloader = torch.utils.data.DataLoader(data_test, batch_size=batch_size, num_workers=5)
+
+# BERT 모델 불러오기
+model = BERTClassifier(bertmodel, dr_rate=0.5).to(device)
+
+# optimizer와 schedule 설정
+no_decay = ['bias', 'LayerNorm.weight']
+optimizer_grouped_parameters = [
+    {'params': [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)], 'weight_decay': 0.01},
+    {'params': [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
+]
+
+optimizer = AdamW(optimizer_grouped_parameters, lr=learning_rate)
+loss_fn = nn.CrossEntropyLoss()
+
+t_total = len(train_dataloader) * num_epochs
+warmup_step = int(t_total * warmup_ratio)
+
+scheduler = get_cosine_schedule_with_warmup(optimizer, num_warmup_steps=warmup_step, num_training_steps=t_total)
+
+# 정확도 측정을 위한 함수 정의
+def calc_accuracy(X, Y):
+    max_vals, max_indices = torch.max(X, 1)
+    train_acc = (max_indices == Y).sum().data.cpu().numpy() / max_indices.size()[0]
+    return train_acc
+
+for e in range(num_epochs):
+    train_acc = 0.0
+    test_acc = 0.0
+    model.train()
+    for batch_id, (token_ids, valid_length, segment_ids, label) in enumerate(tqdm_notebook(train_dataloader)):
+        optimizer.zero_grad()
+        token_ids = token_ids.long().to(device)
+        segment_ids = segment_ids.long().to(device)
+        valid_length = valid_length
+        label = label.long().to(device)
+        out = model(token_ids, valid_length, segment_ids)
+        loss = loss_fn(out, label)
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), max_grad_norm)
+        optimizer.step()
+        scheduler.step()  # Update learning rate schedule
+        train_acc += calc_accuracy(out, label)
+        if batch_id % log_interval == 0:
+            print("epoch {} batch id {} loss {} train acc {}".format(e + 1, batch_id + 1, loss.data.cpu().numpy(),
+                                                                     train_acc / (batch_id + 1)))
+    print("epoch {} train acc {}".format(e + 1, train_acc / (batch_id + 1)))
+
+    model.eval()
+    for batch_id, (token_ids, valid_length, segment_ids, label) in enumerate(tqdm_notebook(test_dataloader)):
+        token_ids = token_ids.long().to(device)
+        segment_ids = segment_ids.long().to(device)
+        valid_length = valid_length
+        label = label.long().to(device)
+        out = model(token_ids, valid_length, segment_ids)
+        test_acc += calc_accuracy(out, label)
+    print("epoch {} test acc {}".format(e + 1, test_acc / (batch_id + 1)))
+
+# 토큰화
+tokenizer = get_tokenizer()
+tok = nlp.data.BERTSPTokenizer(tokenizer, vocab, lower=False)
+
+def predict(predict_sentence):
+    data = [predict_sentence, '0']
+    dataset_another = [data]
+
+    another_test = BERTDataset(dataset_another, 0, 1, tok, max_len, True, False)
+    test_dataloader = torch.utils.data.DataLoader(another_test, batch_size=batch_size, num_workers=5)
+
+    model.eval()
+
+    for batch_id, (token_ids, valid_length, segment_ids, label) in enumerate(test_dataloader):
+        token_ids = token_ids.long().to(device)
+        segment_ids = segment_ids.long().to(device)
+
+        valid_length = valid_length
+        label = label.long().to(device)
+
+        out = model(token_ids, valid_length, segment_ids)
+
+        test_eval = []
+        for i in out:
+            logits = i
+            logits = logits.detach().cpu().numpy()
+
+            if np.argmax(logits) == 0:
+                test_eval.append("IT과학 / 경제 / 사회 / 생활문화 중 하나입니다.")
+            elif np.argmax(logits) == 1:
+                test_eval.append("세계 / 스포츠 / 정치 중 하나입니다.")
+
+        print(test_eval[0])
+
+#질문 무한반복하기! 0 입력시 종료
+end = 1
+while end == 1 :
+    sentence = input("하고싶은 말을 입력해주세요 : ")
+    if sentence == 0 :
+        break
+    predict(sentence)
+    print("\n")
